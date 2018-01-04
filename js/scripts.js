c9c465652099aa9e7eef143f31e21ee91d29b75a
xenogonx <pablez@wp.pl>
2018-01-04 14:53:46 +0100

    css-change-for-mobile

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ let button = document.getElementById('get-card')
 button.addEventListener('click', function() {
   getCard()
   document.getElementById('loader').style.visibility = 'visible';
-  window.location.href="#display";
+  window.location.href="#pic";
 })
 
 // ładowanie losowej karty po załadowaniu strony + loader
