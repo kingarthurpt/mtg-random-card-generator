41d98a7e9024b6e6519a82625da9d0bc9641e867
xenogonx <pablez@wp.pl>
2018-01-06 15:14:05 +0100

    small css changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,69 @@
 
-// zmienna url zawiera pełny adres do losowej karty
-const url = 'https://api.scryfall.com/cards/random'
+// adres API
+const url = 'https://api.scryfall.com/cards/random';
 
-// podpinamy pod guzik nasłuchiwanie na kliknięcie tak aby każdorazowo pobierała nam się losowa karta + włączanie widoczności loadera
-let button = document.getElementById('get-card')
+// podpinamy pod guzik nasłuchiwanie na kliknięcie tak aby każdorazowo po kliknięciu pobierała nam się losowa karta (funkcja getCard) + loader
+let button = document.getElementById('get-card');
 button.addEventListener('click', function() {
-  getCard()
   document.getElementById('loader').style.visibility = 'visible';
   window.location.href="#loader";
-})
+  getCard();
+});
 
-// ładowanie losowej karty po załadowaniu strony + loader
+// wywołanie funkcji getCard po załadowaniu strony + loader
 document.body.onload = function() {
-  getCard()
   document.getElementById('loader').style.visibility = 'visible';
-}
+  getCard();
+};
 
-let nazwa = document.getElementById('card')
-let artist = document.getElementById('artist')
-let art = document.getElementById('pic')
-let type = document.getElementById('type')
-let price = document.getElementById('price')
-let rarity = document.getElementById('rarity')
-let set = document.getElementById('set')
-let shop = document.getElementById('shopping')
-let oracle = document.getElementById('oracle')
+let nazwa = document.getElementById('card');
+let artist = document.getElementById('artist');
+let art = document.getElementById('pic');
+let type = document.getElementById('type');
+let price = document.getElementById('price');
+let rarity = document.getElementById('rarity');
+let set = document.getElementById('set');
+let shop = document.getElementById('shopping');
+let oracle = document.getElementById('oracle');
 
-// implementacja funkcji getcard
+// implementacja funkcji getCard() - połączenie z API, odebranie danych JSON, parsowanie i wyświetlenie
 function getCard() {
-  let xhr = new XMLHttpRequest()
-  xhr.open('GET', url, true)
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
   xhr.addEventListener('load', function() {
     const response = JSON.parse(xhr.response);
+    
+    nazwa.innerHTML = response.name;
+    artist.innerHTML = "<span class='card-name'>autor ilustracji: </span>" + response.artist;
+    type.innerHTML = "<span class='card-name'>typ: </span>" + response.type_line;
+    price.innerHTML = "<span class='card-name'>średnia cena to: </span>" + response.eur + ' €';
+    rarity.innerHTML = "<span class='card-name'>rzadkość: </span>" + response.rarity;
+    set.innerHTML = "<span class='card-name'>dodatek: </span>" + response.set_name + ' [' + response.set + ']';
+    art.src = response.image_uris.normal;
+    oracle.innerHTML = "<span class='card-name'>tekst: </span>" + response.oracle_text;
 
-    nazwa.innerHTML = response.name
-    artist.innerHTML = "<span class='card-name'>autor ilustracji: </span>" + response.artist
-    type.innerHTML = "<span class='card-name'>typ: </span>" + response.type_line
-    price.innerHTML = "<span class='card-name'>średnia cena to: </span>" + response.eur + ' €'
-    rarity.innerHTML = "<span class='card-name'>rzadkość: </span>" + response.rarity
-    set.innerHTML = "<span class='card-name'>dodatek: </span>" + response.set_name + ' [' + response.set + ']'
-    art.src = response.image_uris.normal
-    oracle.innerHTML = "<span class='card-name'>tekst: </span>" + response.oracle_text
-
-    // sprawdzamy czy karta ma cenę, jeżeli nie ma wyświetla się stosowny komunikat
+    // sprawdzamy czy jest dostępna cena dla wylosowanej larty
     if (response.eur === undefined) {
-      price.innerHTML = 'brak ceny'
+      price.innerHTML = 'brak ceny';
     }
+    
     // sprawdzamy rzadkość karty i kolorujemy
     if (response.rarity === 'rare') {
-      rarity.innerHTML = "<span class='card-name'>rzadkość: </span>" + "<span class='rare'>rare </span>"
+      rarity.innerHTML = "<span class='card-name'>rzadkość: </span>" + "<span class='rare'>rare </span>";
     } else if (response.rarity === 'uncommon') {
-      rarity.innerHTML = "<span class='card-name'>rzadkość: </span>" + "<span class='uncommon'>uncommon </span>"
+      rarity.innerHTML = "<span class='card-name'>rzadkość: </span>" + "<span class='uncommon'>uncommon </span>";
     } else if (response.rarity === 'mythic') {
-      rarity.innerHTML = "<span class='card-name'>rzadkość: </span>" + "<span class='mythic'>mythic </span>"
+      rarity.innerHTML = "<span class='card-name'>rzadkość: </span>" + "<span class='mythic'>mythic </span>";
     }
 
     // poniżej generujemy linka do zakupów w magic card market
-    let url = response.purchase_uris.magiccardmarket
-    let shopping = 'kup tą kartę w magic card market'
-    shop.innerHTML = shopping.link(url)
+    let url = response.purchase_uris.magiccardmarket;
+    let shopping = 'kup tą kartę w magic card market';
+    shop.innerHTML = shopping.link(url);
 
-    console.log(response)
-    document.getElementById('loader').style.visibility = 'hidden'
-  })
-  xhr.send()
+    console.log(response);
+    // ukrycie loadera
+    document.getElementById('loader').style.visibility = 'hidden';
+  });
+  xhr.send();
 }
